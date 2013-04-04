457cd9a96d6410414172d1dd3ff859512a7f4190
Brian Dunnette <brian@dunnette.us>
2013-04-04 12:44:02 -0500

    initial person entry form

--- scripts/replicate-iriscouch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -H 'Content-Type: application/json' -X POST http://localhost:5984/_replicate -d ' {"source": "http://fgtc.iriscouch.com/frontdesk", "target": "frontdesk", "create_target": true}'
-chromium-browser http://localhost:5984/frontdesk/_design/frontdesk/_rewrite/
-
